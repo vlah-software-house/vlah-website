12e288d9c7c94d90fe3425c0cf55466a92c8ace1
Dragos Vilcu <vilcu.petre@gmail.com>
2024-05-06 15:41:34 +0200

    update content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ function Clients() {
       <Container>
         <FadeIn className="flex items-center gap-x-8">
           <h2 className="text-center font-display text-sm font-semibold tracking-wider text-white sm:text-left">
-            We’ve worked with hundreds of amazing people
+            We’ve worked with hundreds of amazing people.
           </h2>
           <div className="h-px flex-auto bg-neutral-800" />
         </FadeIn>
